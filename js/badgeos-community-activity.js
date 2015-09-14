09ff1f917cd0bf123a893ed89b372d546ae8bf98
Sym-O <syousfim@gmail.com>
2015-09-14 10:30:59 +0200

    bug fix: rename a variable to a unique name to avoid conflict

--- js/badgeos-community-activity.js
+++ js/badgeos-community-activity.js
@@ -1,14 +1,14 @@
-var idKey = "earner-description-";
+var idEarnerKey = "earner-description-";
 function displayEarnerDescription(e){
     jQuery(".badgeos-earner-description").hide();
-    var elt = jQuery("#"+ idKey + e);
+    var elt = jQuery("#"+ idEarnerKey + e);
     var badgeElt = jQuery("#" + "badgeos-earner-item-" + e);
     elt.css('top'   , badgeElt.position().top   + badgeElt.width());
     elt.css('left'  , badgeElt.position().left + (badgeElt.width() - elt.width())/2 );
     elt.fadeIn("fast");
 }
 function hideEarnerDescription(e){
-    var elt = jQuery("#"+ idKey + e);
+    var elt = jQuery("#"+ idEarnerKey + e);
     elt.stop();
     jQuery(".badgeos-earner-description").hide();
 }
