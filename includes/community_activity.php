df4bc474be6096156b803099752b2d105db5439b
Sym-O <syousfim@gmail.com>
2016-01-21 17:14:42 +0100

    bug fix in querying the last earnings

--- includes/community_activity.php
+++ includes/community_activity.php
@@ -15,26 +15,15 @@
 * 
 */
 function badgeos_get_last_earnings ( $topCount=10 ) { 		
+    $args = array(
+        'post_type'         => 'badgeos-log-entry',
+        'posts_per_page'    => $topCount,
+        's'                 => 'unlocked',
+        'post_status'       => 'publish',
+        'orderby'           => 'date', 
+        'order'             => 'DESC'        
+    );
+    $res = new WP_Query($args);
 
- $args = array(
-	'posts_per_page'   => $topCount,
-	'offset'           => 0,
-	'category'         => '',
-	'category_name'    => '',
-	'orderby'          => 'date',
-	'order'            => 'DESC',
-	'include'          => '',
-	'exclude'          => '',
-	'meta_key'         => '',
-	'meta_value'       => '',
-	'post_type'        => 'badgeos-log-entry',
-	'post_mime_type'   => '',
-	'post_parent'      => '',
-	'author'	       => '',
-	'post_status'      => 'publish',
-	'suppress_filters' => true 
- );
- $res = get_posts( $args );
-
- return  $res;
+    return  $res->posts;
 }
